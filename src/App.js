f09c01ff0a1f9ae6699220221ba619050b3758ad
gines18 <ruczkowski.m@gmail.com>
2023-05-02 11:39:02 +0100

    add Welcome section with a title

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import Navbar from './components/Navbar'
 import Carousel from './components/Carousel'
+import Welcome from'./components/Welcome.jsx'
 
 function App() {
   return (<>
 <Navbar />
 <Carousel />
+<Welcome />
 </>
   )
 
---
+++ src/components/Welcome.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import "./Welcome.css";
+
+function Welcome() {
+  return (
+    <>
+      <div className="welcome">
+        <h1>
+          Welcome to our digital agency where we leverage{" "}
+          <span>technology</span> and <span id="red">c</span>
+          <span id="green">r</span>
+          <span id="blue">e</span>
+          <span id="black">a</span>
+          <span id="pink">t</span>
+          <span id="violet1">i</span>
+          <span id="grey">v</span>
+          <span id="lime">i</span>
+          <span id="violet">t</span>
+          <span id="green">y</span> to help you achieve your business goals.
+        </h1>
+      </div>
+      <div>
+        <img src="" alt="" />
+        <img src="" alt="" />
+        <img src="" alt="" />
+      </div>
+    </>
+  );
+}
+
+export default Welcome;
+
