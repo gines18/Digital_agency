5bc14961ae005154530a9a53353f1659a24e91ef
gines18 <ruczkowski.m@gmail.com>
2023-05-03 19:00:53 +0100

    add icons to navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,25 +1,30 @@
-import React from 'react';
-import './Navbar.css'
+import React from "react";
+import "./Navbar.css";
 
 function Navbar() {
   return (
     <>
-    <div className="container">
-    <div>
-     <img src="./download.png" alt="logo" />
-     </div>
+      <div className="container">
+        <div>
+          <img id="logo" src="./download.png" alt="logo" />
+        </div>
 
-     <div className='navbar-list'>
-        <ul>
-            <li>Home</li>
-            <li>About</li>
-            <li>Contact</li>
-        </ul>
-     </div>
-     </div>
-     
-     </>
-  )
+        <div className="navbar-list">
+          <ul>
+            <li>
+              <img src="./home.png" alt="home"></img>
+            </li>
+            <li>
+              <img src="./information.png" alt="information"></img>
+            </li>
+            <li>
+              <img src="./mail.png" alt="mail"></img>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default Navbar;
