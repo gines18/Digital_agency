8405cfae254dc8d1838959b8082d4a27cb102081
gines18 <ruczkowski.m@gmail.com>
2023-05-03 11:48:21 +0100

    applied Offer images and text media query for below 820px

--- src/components/Offer.jsx
+++ src/components/Offer.jsx
@@ -6,17 +6,17 @@ import './Offer.css'
     {
       id: 1,
       image: 'coffee2.jpeg', 
-      text: 'Coffee is more than just a beverage, it is a culture - a way of life that brings people together and fuels creativity and inspiration'
+      text: 'Coffee is more than just a beverage, it is a culture - a way of life that brings people together and fuels creativity and inspiration.'
     },
     {
       id: 2,
       image: 'coffee1.jpeg', 
-      text: 'Coffee is a universal language that transcends borders and brings people from all walks of life together, united by the simple pleasure of a great cup of coffee'
+      text: 'Coffee is a universal language that transcends borders and brings people from all walks of life together, united by the simple pleasure of a great cup of coffee.'
     },
     {
       id: 3,
       image: 'coffee3.jpeg', 
-      text: 'There is nothing quite like the aroma of freshly brewed coffee - the rich, earthy scent that awakens your senses and invites you to savor every sip'
+      text: 'There is nothing quite like the aroma of freshly brewed coffee - the rich, earthy scent that awakens your senses and invites you to savor every sip.'
     }
     
 
