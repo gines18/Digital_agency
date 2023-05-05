b77bf23f6e2e4a4b7636cccf228c6867120a34f2
gines18 <ruczkowski.m@gmail.com>
2023-05-05 11:16:25 +0100

    removed Question component from App

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Welcome from'./components/Welcome.jsx'
 import MyComponent from './components/Offer'
 import Footer from './components/Footer.jsx'
 import Coffee from './components/RecipeAPI.jsx'
-import Question from './components/Question.jsx'
+
 import ContactCard from './components/ContactForm.jsx'
 
 
@@ -18,7 +18,7 @@ function App() {
 <Carousel />
 <Coffee />
 <ContactCard />
-<Question />
+
 <Footer/>
 </>
   )
