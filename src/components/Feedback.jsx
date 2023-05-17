5e4d5f06376353f252ba90acd24a062ce4d8f1e7
gines18 <ruczkowski.m@gmail.com>
2023-05-17 18:45:26 +0100

    change Welcome section padding, add background color for Feedback section  <p> tag

--- src/components/Feedback.jsx
+++ src/components/Feedback.jsx
@@ -13,8 +13,8 @@ function Feedback() {
   return (
     <div className='container-feedback'>
       <h2>THANKS FOR COMING</h2>
-      <p>We'd love to hear you thoughts about the website.</p>
-      <button className='button-feedback' onClick={handleClick}>Give feedback</button>
+      <p>We'd love to hear your thoughts about the website</p>
+      <button className='button-feedback' onClick={handleClick}>Leave feedback</button>
       <div className={showAlert ? 'alert show' : 'alert'} role="alert">
         Survey coming soon!
       </div>
