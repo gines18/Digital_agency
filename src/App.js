76f275821dc2cef12f4bb746bb4ab557441e653b
gines18 <ruczkowski.m@gmail.com>
2023-06-11 10:46:46 +0100

    deleted component

--- src/App.js
+++ src/App.js
@@ -5,10 +5,7 @@ import Welcome from "./components/Welcome.jsx";
 import MyComponent from "./components/Offer";
 import Footer from "./components/Footer.jsx";
 import Coffee from "./components/RecipeAPI.jsx";
-import CoffeeRestaurant from "./components/CoffeeRestaurant.jsx";
-
 import Article from "./components/Article.jsx";
-
 import ContactCard from "./components/ContactForm.jsx";
 
 import Feedback from "./components/Feedback.jsx";
@@ -21,7 +18,6 @@ function App() {
       <MyComponent />
       <Carousel />
       <Article />
-      <CoffeeRestaurant />
       <Coffee />
       <Feedback />
       <ContactCard />
