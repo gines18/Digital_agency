301ac89156f60dfa1e87409bd000f61b2fbb816d
gines18 <ruczkowski.m@gmail.com>
2023-06-27 15:03:17 +0100

    add h1 on main site

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -3,8 +3,9 @@ import { Routes, Route } from "react-router-dom";
 import About from "./About";
 import Home from "./Home";
 import Products from "./Products";
-
-
+import './Layout.css'
+import building from './assets/building.jpg'
+import Footer from "./Footer.jsx";
 function Layout() {
   return (
     <>
@@ -13,6 +14,12 @@ function Layout() {
       <Route path="/home" element={<Home />} />
       <Route path="/products" element={<Products />} />
     </Routes>
+
+<div className="main-site-box">
+  <h1>Building ...</h1>
+  </div>
+
+
 
     </>
   );
